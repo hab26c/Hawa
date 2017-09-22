6e2a8f64f3ddfb5a768871caa58a7b0332dd6378
Hawa <hab26c@mail.missouri.edu>
2017-09-22 11:21:23 -0500

    Area changed to A

---
+++ Problem1.c
@@ -0,0 +1,29 @@
+/*
+ ============================================================================
+ Name        : Problem 1
+ Author      : Hawa Benshatwan
+ Version     :
+ Copyright   : Your copyright notice
+ Description : calculate the area of a circle
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define PI 3.14  //PI value is defined as 3.14
+
+ int main ()
+
+{
+    float r ;  //r = radius
+    float Area ;
+    printf("Enter the radius value:") ;
+    scanf("%f", &r) ;  //save the entered value to r
+    Area= PI*r*r ;  //do the calculation for the area
+
+    printf("The area of the circle = %f", Area) ;
+
+}
+
